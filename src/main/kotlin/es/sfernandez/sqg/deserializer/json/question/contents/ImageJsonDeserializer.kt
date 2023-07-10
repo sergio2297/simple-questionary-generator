9519d6eaa2d39800773e923b39b841ed64abca15
sfernandez <sergio_2297@hotmail.es>
2023-07-10 20:14:58 +0200

    refactor: ImageJsonDeserializer improved by refactoring extraction of properties

--- src/main/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ImageJsonDeserializer.kt
+++ src/main/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ImageJsonDeserializer.kt
@@ -2,7 +2,6 @@ package es.sfernandez.sqg.deserializer.json.question.contents
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import es.sfernandez.sqg.deserializer.json.JsonDeserializer
 import es.sfernandez.sqg.deserializer.json.JsonKeys
@@ -18,26 +17,10 @@ class ImageJsonDeserializer : JsonDeserializer<Image>(Image::class.java) {
         override fun deserialize(parser: JsonParser?, ctxt: DeserializationContext?): Image {
             val node = extractJsonNode(parser)
 
-            val path = extractPath(node)
-            val clickToSee = extractClickToSee(node)
+            val path = extractText(node, JsonKeys.Image.PATH)
+            val clickToSee = extractBool(node, JsonKeys.Image.CLICK_TO_SEE)
 
             return Image(path, clickToSee)
-        }
-
-        private fun extractPath(node: JsonNode): String {
-            return try {
-                node[JsonKeys.Image.PATH].asText()
-            } catch (ex: NullPointerException) {
-                ""
-            }
-        }
-
-        private fun extractClickToSee(node: JsonNode): Boolean {
-            return try {
-                node[JsonKeys.Image.CLICK_TO_SEE].asBoolean()
-            } catch (ex: NullPointerException) {
-                false
-            }
         }
 
     }
--- src/test/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ImageJsonDeserializerTest.kt
+++ src/test/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ImageJsonDeserializerTest.kt
@@ -4,6 +4,7 @@ import es.sfernandez.sqg.BasicFixtures
 import es.sfernandez.sqg.deserializer.json.JsonDeserializer
 import es.sfernandez.sqg.deserializer.json.JsonFixtures
 import es.sfernandez.sqg.deserializer.json.JsonKeys
+import es.sfernandez.sqg.deserializer.logs.DeserializationLog
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -60,6 +61,53 @@ class ImageJsonDeserializerTest {
         val image = deserializer.deserialize(json)
 
         assertThat(image.clickToSee).isEqualTo(clickToSee)
+    }
+
+    private fun checkDeserializerLogsContainsWarningWithKey(key: String) {
+        assertThat(deserializer.logs.asSequence()
+            .any { log -> log.level == DeserializationLog.Level.WARNING
+                    && log.msg.contains(key)})
+            .isTrue()
+    }
+
+    @Test
+    fun logs_afterDeserializeJson_withoutPath_containsWarningMsgTest() {
+        val json = JsonFixtures.EMPTY_JSON_OBJECT
+
+        deserializer.deserialize(json)
+
+        checkDeserializerLogsContainsWarningWithKey(JsonKeys.Image.PATH)
+    }
+
+    @Test
+    fun logs_afterDeserializeJson_withIncorrectTypePath_containsWarningMsgTest() {
+        val json = """
+            { "${JsonKeys.Image.PATH}": {}}
+        """.trimIndent()
+
+        deserializer.deserialize(json)
+
+        checkDeserializerLogsContainsWarningWithKey(JsonKeys.Image.PATH)
+    }
+
+    @Test
+    fun logs_afterDeserializeJson_withoutClickToSee_containsWarningMsgTest() {
+        val json = JsonFixtures.EMPTY_JSON_OBJECT
+
+        deserializer.deserialize(json)
+
+        checkDeserializerLogsContainsWarningWithKey(JsonKeys.Image.CLICK_TO_SEE)
+    }
+
+    @Test
+    fun logs_afterDeserializeJson_withIncorrectTypeClickToSee_containsWarningMsgTest() {
+        val json = """
+            { "${JsonKeys.Image.CLICK_TO_SEE}": "true"}
+        """.trimIndent()
+
+        deserializer.deserialize(json)
+
+        checkDeserializerLogsContainsWarningWithKey(JsonKeys.Image.CLICK_TO_SEE)
     }
 
 }
