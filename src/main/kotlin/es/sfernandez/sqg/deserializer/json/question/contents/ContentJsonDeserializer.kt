5b2b69a570564273f2289a6f60855d1cd060e814
Sfernandez <sergio_2297@hotmail.es>
2023-07-13 19:10:27 +0200

    feat: new ContentJsonDeserializer created

---
+++ src/main/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ContentJsonDeserializer.kt
@@ -0,0 +1,103 @@
+package es.sfernandez.sqg.deserializer.json.question.contents
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import es.sfernandez.sqg.beans.contents.Content
+import es.sfernandez.sqg.beans.contents.ContentType
+import es.sfernandez.sqg.beans.contents.UnknownContent
+import es.sfernandez.sqg.deserializer.DeserializationException
+import es.sfernandez.sqg.deserializer.json.JsonDeserializer
+
+class ContentJsonDeserializer : JsonDeserializer<Content> {
+
+    //---- Attributes ----
+    private val textDeserializer : TextJsonDeserializer
+    private val soundDeserializer : SoundJsonDeserializer
+    private val videoDeserializer : VideoJsonDeserializer
+    private val imageDeserializer : ImageJsonDeserializer
+
+    //---- Constructor ----
+    constructor() : super(Content::class.java) {
+        textDeserializer = TextJsonDeserializer()
+        soundDeserializer = SoundJsonDeserializer()
+        videoDeserializer = VideoJsonDeserializer()
+        imageDeserializer = ImageJsonDeserializer()
+    }
+
+    internal constructor(textDeserializer: TextJsonDeserializer,
+                         soundDeserializer: SoundJsonDeserializer,
+                         videoDeserializer: VideoJsonDeserializer,
+                         imageDeserializer: ImageJsonDeserializer
+    ) : super(Content::class.java) {
+        this.textDeserializer = textDeserializer
+        this.soundDeserializer = soundDeserializer
+        this.videoDeserializer = videoDeserializer
+        this.imageDeserializer = imageDeserializer
+    }
+
+    //---- Methods ----
+    override fun createDeserializer() : StdDeserializer<Content> {
+        return CustomDeserializer()
+    }
+
+    private inner class CustomDeserializer : StdDeserializer<Content>(mappedClass) {
+        override fun deserialize(parser: JsonParser?, ctxt: DeserializationContext?) : Content {
+            val jsonNode = extractJsonNode(parser)
+
+            if(!jsonNode.isObject) {
+                log.warning("Error. ${ContentJsonDeserializer::class.simpleName} can only deserialize json objects")
+                return UnknownContent()
+            }
+
+            if(!nodeIsValidContentType(jsonNode)) {
+                log.warning("Error. ${ContentJsonDeserializer::class.simpleName} can't deserialize json received." +
+                        " This may be due to an incorrect content key or a json object with more " +
+                        "than one content json object. Unknown content returned instead.")
+                return UnknownContent()
+            }
+
+            val concreteDeserializer = deserializerFor(contentTypeByJsonName(firstFieldName(jsonNode)))
+            val content = concreteDeserializer.deserialize(jsonNode[firstFieldName(jsonNode)].toString())
+            dumpLogsFrom(concreteDeserializer)
+
+            return content
+        }
+
+    }
+
+    fun nodeIsValidContentType(jsonNode : JsonNode) : Boolean {
+        val validJsonNames = validContentTypes().map { type -> type.jsonName }
+        return nodeHasOnlyOneField(jsonNode)
+                && validJsonNames.contains(firstFieldName(jsonNode))
+    }
+
+    private fun validContentTypes() : Iterable<ContentType> {
+        return enumValues<ContentType>().filter { type -> type != ContentType.UNKNOWN }
+    }
+
+    private fun nodeHasOnlyOneField(jsonNode : JsonNode) : Boolean {
+        return jsonNode.fieldNames().asSequence().count() == 1
+    }
+
+    private fun firstFieldName(jsonNode : JsonNode) : String {
+        return jsonNode.fieldNames().asSequence().first()
+    }
+
+    private fun contentTypeByJsonName(name: String): ContentType {
+        return enumValues<ContentType>().first { value -> value.jsonName == name }
+    }
+
+    private fun deserializerFor(contentType : ContentType) : JsonDeserializer<out Content> {
+        return when (contentType) {
+            ContentType.TEXT -> textDeserializer
+            ContentType.SOUND -> soundDeserializer
+            ContentType.VIDEO -> videoDeserializer
+            ContentType.IMAGE -> imageDeserializer
+            else -> throw DeserializationException("Error. There is no ${JsonDeserializer::class.simpleName} available " +
+                    "for content type ${contentType.name}")
+        }
+    }
+
+}
---
+++ src/test/kotlin/es/sfernandez/sqg/deserializer/json/question/contents/ContentJsonDeserializerTest.kt
@@ -0,0 +1,244 @@
+package es.sfernandez.sqg.deserializer.json.question.contents
+
+import es.sfernandez.sqg.beans.contents.*
+import es.sfernandez.sqg.deserializer.json.JsonFixtures
+import es.sfernandez.sqg.deserializer.logs.DeserializationLog
+import es.sfernandez.sqg.deserializer.logs.DeserializationLogUtilsForTests
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+class ContentJsonDeserializerTest {
+
+    //---- Attributes ----
+    private var deserializer = ContentJsonDeserializer()
+
+    private lateinit var textJsonDeserializer: TextJsonDeserializer
+    private lateinit var soundJsonDeserializer: SoundJsonDeserializer
+    private lateinit var videoJsonDeserializer: VideoJsonDeserializer
+    private lateinit var imageJsonDeserializer: ImageJsonDeserializer
+
+    //---- Fixtures ----
+    private val deserializedText = Text()
+    private val deserializedSound = Sound()
+    private val deserializedVideo = Video()
+    private val deserializedImage = Image()
+
+    //---- Configuration ----
+    @BeforeEach
+    fun setup() {
+        deserializer = createMockedDeserializer()
+    }
+
+    //---- Methods ----
+    private fun createNormalDeserializer() : ContentJsonDeserializer {
+        return ContentJsonDeserializer()
+    }
+
+    private fun createMockedDeserializer() : ContentJsonDeserializer {
+        textJsonDeserializer = Mockito.mock(TextJsonDeserializer::class.java)
+        soundJsonDeserializer = Mockito.mock(SoundJsonDeserializer::class.java)
+        videoJsonDeserializer = Mockito.mock(VideoJsonDeserializer::class.java)
+        imageJsonDeserializer = Mockito.mock(ImageJsonDeserializer::class.java)
+        return ContentJsonDeserializer(textJsonDeserializer, soundJsonDeserializer,
+            videoJsonDeserializer, imageJsonDeserializer)
+    }
+
+    private fun mockDeserializersToReturnEmptyLogs() {
+        Mockito.lenient().`when`(textJsonDeserializer.logs()).thenReturn(arrayOf())
+        Mockito.lenient().`when`(soundJsonDeserializer.logs()).thenReturn(arrayOf())
+        Mockito.lenient().`when`(videoJsonDeserializer.logs()).thenReturn(arrayOf())
+        Mockito.lenient().`when`(imageJsonDeserializer.logs()).thenReturn(arrayOf())
+    }
+
+    //---- Tests ----
+    @Test
+    fun deserialize_notJsonObject_returnUnknownContentTest() {
+        val json = JsonFixtures.EMPTY_JSON_ARRAY
+        deserializer = createNormalDeserializer()
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isInstanceOf(UnknownContent::class.java)
+    }
+
+    @Test
+    fun afterDeserialize_notJsonObject_logsHaveWarningTest() {
+        val json = JsonFixtures.EMPTY_JSON_ARRAY
+        deserializer = createNormalDeserializer()
+
+        deserializer.deserialize(json)
+
+        DeserializationLogUtilsForTests.checkDeserializerLogsContainsWarningWithWord(
+            deserializer, ContentJsonDeserializer::class.java.simpleName)
+    }
+
+    @Test
+    fun deserialize_objectWithNotValidName_returnUnknownContentTest() {
+        val json = """
+             {
+                 "not_content_type": {}
+             }"""
+        deserializer = createNormalDeserializer()
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isInstanceOf(UnknownContent::class.java)
+    }
+
+    @Test
+    fun afterDeserialize_objectWithNotValidName_logsHaveWarningTest() {
+        val json = """
+             {
+                 "not_content_type": {}
+             }"""
+        deserializer = createNormalDeserializer()
+
+        deserializer.deserialize(json)
+
+        DeserializationLogUtilsForTests.checkDeserializerLogsContainsWarningWithWord(
+            deserializer, ContentJsonDeserializer::class.java.simpleName)
+    }
+
+    @Test
+    fun deserialize_objectWithUnknownContentType_returnUnknownContentTest() {
+        val json = """
+             {
+                "${ContentType.UNKNOWN.jsonName}": {}
+             }
+             """
+        deserializer = createNormalDeserializer()
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isInstanceOf(UnknownContent::class.java)
+    }
+
+    @Test
+    fun afterDeserialize_objectWithUnknownContentType_logsContainWarningTest() {
+        val json = """
+             {
+                "${ContentType.UNKNOWN.jsonName}": {}
+             }
+             """
+        deserializer = createNormalDeserializer()
+
+        deserializer.deserialize(json)
+
+        DeserializationLogUtilsForTests.checkDeserializerLogsContainsWarningWithWord(
+            deserializer, ContentJsonDeserializer::class.java.simpleName)
+    }
+
+    @Test
+    fun deserialize_objectWithMoreThanOneField_returnUnknownContentTest() {
+        val json = """
+             {
+                "${ContentType.TEXT.jsonName}": {},
+                "${ContentType.IMAGE.jsonName}": {}
+             }
+             """
+        deserializer = createNormalDeserializer()
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isInstanceOf(UnknownContent::class.java)
+    }
+
+    @Test
+    fun afterDeserialize_objectWithMoreThanOneField_logsContainWarningTest() {
+        val json = """
+             {
+                "${ContentType.TEXT.jsonName}": {},
+                "${ContentType.IMAGE.jsonName}": {}
+             }
+             """
+        deserializer = createNormalDeserializer()
+
+        deserializer.deserialize(json)
+
+        DeserializationLogUtilsForTests.checkDeserializerLogsContainsWarningWithWord(
+            deserializer, ContentJsonDeserializer::class.java.simpleName)
+    }
+
+    @Test
+    fun deserialize_objectWithTextContent_returnTextTest() {
+        mockDeserializersToReturnEmptyLogs()
+        Mockito.`when`(textJsonDeserializer.deserialize(Mockito.anyString())).thenReturn(deserializedText)
+        val json = """
+            {
+                "${ContentType.TEXT.jsonName}": {}
+            }
+            """
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isEqualTo(deserializedText)
+    }
+
+    @Test
+    fun deserialize_objectWithSoundContent_returnSoundTest() {
+        mockDeserializersToReturnEmptyLogs()
+        Mockito.`when`(soundJsonDeserializer.deserialize(Mockito.anyString())).thenReturn(deserializedSound)
+        val json = """
+            {
+                "${ContentType.SOUND.jsonName}": {}
+            }
+            """
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isEqualTo(deserializedSound)
+    }
+
+    @Test
+    fun deserialize_objectWithVideoContent_returnVideoTest() {
+        mockDeserializersToReturnEmptyLogs()
+        Mockito.`when`(videoJsonDeserializer.deserialize(Mockito.anyString())).thenReturn(deserializedVideo)
+        val json = """
+            {
+                "${ContentType.VIDEO.jsonName}": {}
+            }
+            """
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isEqualTo(deserializedVideo)
+    }
+
+    @Test
+    fun deserialize_objectWithImageContent_returnImageTest() {
+        mockDeserializersToReturnEmptyLogs()
+        Mockito.`when`(imageJsonDeserializer.deserialize(Mockito.anyString())).thenReturn(deserializedImage)
+        val json = """
+            {
+                "${ContentType.IMAGE.jsonName}": {}
+            }
+            """
+
+        val content = deserializer.deserialize(json)
+
+        assertThat(content).isEqualTo(deserializedImage)
+    }
+
+    private fun mockSomeDeserializationLog() : Array<DeserializationLog> {
+        return generateSequence { Mockito.mock(DeserializationLog::class.java) }
+            .take(3).toList().toTypedArray()
+    }
+
+    @Test
+    fun afterDeserialize_deserializerContainsLogsProducedBy_usedDeserializerTest() {
+        val textLogs = mockSomeDeserializationLog()
+        Mockito.`when`(textJsonDeserializer.logs()).thenReturn(textLogs)
+
+        val json = """
+            {
+                "${ContentType.TEXT.jsonName}": {}
+            }"""
+
+        deserializer.deserialize(json)
+
+        assertThat(deserializer.logs()).contains(*textLogs)
+    }
+
+}
