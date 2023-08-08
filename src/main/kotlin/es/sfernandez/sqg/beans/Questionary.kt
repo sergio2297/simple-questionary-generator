37c631593940e2f359caae9466582441d8be2ea9
Sfernandez <sergio_2297@hotmail.es>
2023-08-09 00:51:22 +0200

    feat: title and portrait added to Questionary bean

--- src/main/kotlin/es/sfernandez/sqg/beans/Questionary.kt
+++ src/main/kotlin/es/sfernandez/sqg/beans/Questionary.kt
@@ -1,7 +1,10 @@
 package es.sfernandez.sqg.beans
 
+import es.sfernandez.sqg.beans.contents.Image
 import es.sfernandez.sqg.beans.question.Question
 
 class Questionary(
-    var questions : Array<Question> = arrayOf()
+    var title: String = "",
+    var portrait: Image = Image(),
+    var questions : Array<Question> = arrayOf(),
 )
--- src/test/kotlin/es/sfernandez/sqg/beans/QuestionaryTest.kt
+++ src/test/kotlin/es/sfernandez/sqg/beans/QuestionaryTest.kt
@@ -1,5 +1,7 @@
 package es.sfernandez.sqg.beans
 
+import es.sfernandez.sqg.BasicFixtures
+import es.sfernandez.sqg.beans.contents.Image
 import es.sfernandez.sqg.beans.question.Question
 import org.assertj.core.api.Assertions.assertThat
 import org.mockito.Mockito
@@ -27,13 +29,41 @@ class QuestionaryTest {
 
     //---- Tests ----
     @Test
+    fun constructor_createsByDefault_emptyTitleTest() {
+        assertThat(questionary.title).isEmpty()
+    }
+
+    @Test
+    fun construct_withTitle_worksTest() {
+        val title = BasicFixtures.SOME_TEXT_1
+
+        questionary = Questionary(title = title)
+
+        assertThat(questionary.title).isEqualTo(title)
+    }
+
+    @Test
+    fun constructor_createsByDefault_emptyPortraitTest() {
+        assertThat(questionary.portrait.path).isEmpty()
+    }
+
+    @Test
+    fun construct_withImage_worksTest() {
+        val image = Mockito.mock(Image::class.java)
+
+        questionary = Questionary(portrait = image)
+
+        assertThat(questionary.portrait).isEqualTo(image)
+    }
+
+    @Test
     fun constructor_createsByDefault_emptyQuestionaryTest() {
         assertThat(questionary.questions).isEmpty()
     }
 
     @Test
     fun construct_withArrayOfQuestions_worksTest() {
-        questionary = Questionary(someQuestions)
+        questionary = Questionary(questions = someQuestions)
 
         assertThat(questionary.questions).containsExactly(*someQuestions)
     }
