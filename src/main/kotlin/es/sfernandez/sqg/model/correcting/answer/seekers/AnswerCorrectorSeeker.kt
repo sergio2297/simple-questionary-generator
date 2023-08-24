62302f9b9c5cb09fb49f0e168fc3f18a88042f64
Sfernandez <sergio_2297@hotmail.es>
2023-08-24 23:18:38 +0200

    feat: AnswerCorrectorSeeker created

---
+++ src/main/kotlin/es/sfernandez/sqg/model/correcting/answer/seekers/AnswerCorrectorSeeker.kt
@@ -0,0 +1,23 @@
+package es.sfernandez.sqg.model.correcting.answer.seekers
+
+import es.sfernandez.sqg.beans.question.answers.Answer
+import es.sfernandez.sqg.model.correcting.answer.correctors.AnswerCorrector
+
+/**
+ * An AnswerCorrectorSeeker provides one method that for a given [Answer] return its apropiate [AnswerCorrector]
+ *
+ * @param RESULT type of result of the corrector searched
+ */
+interface AnswerCorrectorSeeker<out RESULT> {
+
+    /**
+     * Seek the apropiate AnswerCorrector for the given answer based on its [AnswerCorrection]
+     *
+     * It may throw an [AnswerCorrectorException]
+     *
+     * @param answer Answer used to search the AnswerCorrector
+     * @return the apropiate AnswerCorrector for the given answer
+     */
+    fun correctorFor(answer: Answer) : AnswerCorrector<RESULT>
+
+}
