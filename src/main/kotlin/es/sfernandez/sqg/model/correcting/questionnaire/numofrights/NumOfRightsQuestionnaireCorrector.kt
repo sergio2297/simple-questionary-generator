11748a397ee9d1700c1399cc03c6e7cd203dfc01
Sfernandez <sergio_2297@hotmail.es>
2023-08-27 10:23:44 +0200

    feat: NumOfRightsQuestionnaireCorrector created

---
+++ src/main/kotlin/es/sfernandez/sqg/model/correcting/questionnaire/numofrights/NumOfRightsQuestionnaireCorrector.kt
@@ -0,0 +1,64 @@
+package es.sfernandez.sqg.model.correcting.questionnaire.numofrights
+
+import es.sfernandez.sqg.beans.Questionnaire
+import es.sfernandez.sqg.beans.question.Question
+import es.sfernandez.sqg.beans.question.answers.Answer
+import es.sfernandez.sqg.beans.question.answers.correction.AnswerCorrection
+import es.sfernandez.sqg.model.correcting.answer.correctors.AnswerCorrector
+import es.sfernandez.sqg.model.correcting.answer.seekers.RightOrNotCorrectorSeeker
+import es.sfernandez.sqg.model.correcting.questionnaire.QuestionnaireCorrector
+import es.sfernandez.sqg.model.correcting.replies.Reply
+
+/**
+ * A NumOfRightsQuestionnaireCorrector is a [QuestionnaireCorrector] that generates a [NumOfRightsQuestionnaireResult] as
+ * result of correcting a [Questionnaire] from replies given to its questions.
+ *
+ * All questionnaire's questions must have answers with [AnswerCorrection.Type.RIGHT_OR_NOT] type. If not, the corrector
+ * wouldn't be able to find an apropiate [AnswerCorrector] and an exception will be thrown
+ *
+ */
+class NumOfRightsQuestionnaireCorrector(
+    questionnaire: Questionnaire,
+) : QuestionnaireCorrector<NumOfRightsQuestionnaireResult>(questionnaire) {
+
+    //---- Attributes ----
+    private val answerCorrectorSeeker = RightOrNotCorrectorSeeker()
+
+    //---- Methods ----
+    override fun generateResult(): NumOfRightsQuestionnaireResult {
+        val questionResults = correctQuestions()
+
+        return NumOfRightsQuestionnaireResult(
+            questionnaire,
+            countNotAnswered(),
+            countRights(questionResults),
+            countFailures(questionResults),
+        )
+    }
+
+    private fun correctQuestions(): List<RightOrNotQuestionResult> {
+        return questionReplies.keys.map(this::correctQuestion).toList()
+    }
+
+    private fun correctQuestion(question: Question) : RightOrNotQuestionResult {
+        val answer = question.answer
+        val reply = replyFor(question)
+        val isRight = correctAnswer(answer, reply)
+
+        return RightOrNotQuestionResult(question, reply, isRight)
+    }
+
+    private fun correctAnswer(answer: Answer, reply: Reply<*>): Boolean {
+        return answerCorrectorSeeker.correctorFor(answer)
+            .correct(answer.correction, reply)
+    }
+
+    private fun countRights(questionResults: List<RightOrNotQuestionResult>): Int {
+        return questionResults.count(RightOrNotQuestionResult::isRight)
+    }
+
+    private fun countFailures(questionResults: List<RightOrNotQuestionResult>): Int {
+        return questionResults.count { result -> !result.isRight }
+    }
+
+}
---
+++ src/test/kotlin/es/sfernandez/sqg/model/correcting/questionnaire/numofrights/NumOfRightsQuestionnaireCorrectorTest.kt
@@ -0,0 +1,162 @@
+package es.sfernandez.sqg.model.correcting.questionnaire.numofrights
+
+import es.sfernandez.sqg.beans.Questionnaire
+import es.sfernandez.sqg.beans.question.Question
+import es.sfernandez.sqg.beans.question.answers.Answer
+import es.sfernandez.sqg.beans.question.answers.correction.rightornot.CorrectValuesRegex
+import es.sfernandez.sqg.beans.question.answers.correction.rightornot.PossibleValues
+import es.sfernandez.sqg.model.correcting.replies.TextReply
+import es.sfernandez.sqg.utilities.Fixtures
+import es.sfernandez.sqg.utilities.mocking.MocksQuestion
+import es.sfernandez.sqg.utilities.mocking.MocksQuestionnaire
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+import kotlin.test.BeforeTest
+
+class NumOfRightsQuestionnaireCorrectorTest {
+
+    //---- Attributes ----
+    private lateinit var corrector: NumOfRightsQuestionnaireCorrector
+
+    //---- Fixtures ----
+    private lateinit var questions: Array<Question>
+
+    //---- Configuration ----
+    @BeforeTest
+    fun setup() {
+        questions = arrayOf(
+            questionWithPossibleValuesCorrection("", Fixtures.SOME_TEXT_1, Fixtures.SOME_TEXT_2),
+            questionWithPossibleValuesCorrection("", Fixtures.SOME_TEXT_1),
+            questionWithRegexValuesCorrection(Regex(".*"))
+        )
+        corrector = NumOfRightsQuestionnaireCorrector(createQuestionnaire(questions))
+    }
+
+    //---- Methods ----
+    companion object : MocksQuestionnaire, MocksQuestion {
+
+        @JvmStatic
+        fun streamOfQuestionnaires(): Stream<Questionnaire> {
+            return Stream.of(
+                mockQuestionnaireWith(arrayOf()),
+                mockQuestionnaireWith(mockQuestions(1)),
+                mockQuestionnaireWith(mockQuestions(20)),
+            )
+        }
+
+        private fun createQuestionnaire(questions: Array<Question>): Questionnaire {
+            val questionnaire = Questionnaire()
+            questionnaire.questions = questions
+            return questionnaire
+        }
+
+        private fun questionWithPossibleValuesCorrection(vararg values: String): Question {
+            val answer = Answer()
+            answer.correction = PossibleValues(*values)
+
+            val question = Question()
+            question.answer = answer
+
+            return question
+        }
+
+        private fun questionWithRegexValuesCorrection(regex: Regex): Question {
+            val answer = Answer()
+            answer.correction = CorrectValuesRegex(regex)
+
+            val question = Question()
+            question.answer = answer
+
+            return question
+        }
+
+    }
+
+    //---- Tests ----
+    @Test
+    fun generatedResultQuestionnaire_isSameAs_correctorsQuestionnaireTest() {
+        val questionnaire = Questionnaire()
+        corrector = NumOfRightsQuestionnaireCorrector(questionnaire)
+
+        val result = corrector.generateResult()
+
+        assertThat(result.questionnaire).isSameAs(questionnaire)
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamOfQuestionnaires")
+    fun generatedResultNumOfQuestions_isSameAs_questionnaireSizeTest(questionnaire: Questionnaire) {
+        corrector = NumOfRightsQuestionnaireCorrector(questionnaire)
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfQuestions).isEqualTo(questionnaire.questions.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamOfQuestionnaires")
+    fun generatedResult_withNoReplies_hasNotAnsweredAtFullTest(questionnaire: Questionnaire) {
+        corrector = NumOfRightsQuestionnaireCorrector(questionnaire)
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfNotAnswered).isEqualTo(result.numOfQuestions)
+    }
+
+    @Test
+    fun generatedResult_withReplies_decreaseNotAnsweredTest() {
+        corrector.registerReply(questions[0], TextReply(Fixtures.EMPTY_TEXT))
+        corrector.registerReply(questions[1], TextReply(Fixtures.EMPTY_TEXT))
+        corrector.registerReply(questions[2], TextReply(Fixtures.SOME_TEXT_1))
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfNotAnswered).isEqualTo(0)
+    }
+
+    @Test
+    fun generatedResult_withRightReplies_countRightsCorrectlyTest() {
+        corrector.registerReply(questions[0], TextReply(Fixtures.EMPTY_TEXT))
+        corrector.registerReply(questions[1], TextReply(Fixtures.SOME_TEXT_2))
+        corrector.registerReply(questions[2], TextReply(Fixtures.SOME_TEXT_1))
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfRights).isEqualTo(2)
+    }
+
+    @Test
+    fun generatedResult_withNoRightReplies_hasZeroRightsCorrectlyTest() {
+        corrector.registerReply(questions[0], TextReply(Fixtures.SOME_TEXT_3))
+        corrector.registerReply(questions[1], TextReply(Fixtures.SOME_TEXT_3))
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfRights).isEqualTo(0)
+    }
+
+    @Test
+    fun generatedResult_withNotRightReplies_countFailuresCorrectlyTest() {
+        corrector.registerReply(questions[0], TextReply(Fixtures.SOME_TEXT_3))
+        corrector.registerReply(questions[1], TextReply(Fixtures.SOME_TEXT_3))
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfFailures).isEqualTo(2)
+    }
+
+    @Test
+    fun generatedResult_withNoFailedReplies_hasZeroFailuresCorrectlyTest() {
+        corrector.registerReply(questions[0], TextReply(Fixtures.EMPTY_TEXT))
+        corrector.registerReply(questions[1], TextReply(Fixtures.EMPTY_TEXT))
+        corrector.registerReply(questions[2], TextReply(Fixtures.SOME_TEXT_1))
+
+        val result = corrector.generateResult()
+
+        assertThat(result.numOfFailures).isEqualTo(0)
+    }
+
+}
